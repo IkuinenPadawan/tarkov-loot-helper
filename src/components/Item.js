ca845920826ea455a929afd4c0fde21cc3896d39
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2021-07-20 14:28:48 +0300

    Add useState to Item component.
    Store data locally for now. Items might have to be
    moved to App.js eventually? Also will be fetched from
    backend in the end.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { React, useState } from 'react'
+
 
 const Item = () => {
+  const [items, setItems] = useState([
+    {
+      name: 'salewa',
+      img: 'img/Salewa.png',
+    },
+  ]);
   return (
     <div>
-      
+      {items.map((item) => (
+        <div>
+          <img src={item.img} alt="" />
+          <p>{item.name}</p>
+        </div>
+      ))}
     </div>
   )
 }
