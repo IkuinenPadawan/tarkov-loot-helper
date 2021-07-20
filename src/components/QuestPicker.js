59dab0108e2161b6c27d7f8cdde6e770014799b5
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2021-07-20 12:56:32 +0300

    Add first three quest checkboxes.

--- src/components/QuestPicker.js
+++ src/components/QuestPicker.js
@@ -3,9 +3,22 @@ import React from 'react'
 const QuestPicker = () => {
   return (
     <div>
-      
+      <h4>Quests completed</h4>
+      <div>
+        <input type='checkbox' id='debut' name='debut' value='Debut' />
+        <label htmlFor='debut'> Debut</label>
+        <input type='checkbox' id='shortage' name='shortage' value='Shortage' />
+        <label htmlFor='shortage'> Shortage</label>
+        <input
+          type='checkbox'
+          id='acquintance'
+          name='acquintance'
+          value='Acquintance'
+        />
+        <label htmlFor='acquintance'> Acquintance</label>
+      </div>
     </div>
-  )
+  );
 }
 
 export default QuestPicker
