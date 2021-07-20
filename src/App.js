39c51f8090644875f9ddf8fbe4ab499ce74c3d20
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2021-07-20 12:36:36 +0300

    Create QuestPicker component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header'
 import LevelPicker from './components/LevelPicker'
+import QuestPicker from './components/QuestPicker';
 
 function App() {
   return (
     <div className="App">
       <Header title='Tarkov Loot Tool'/>
       <LevelPicker levels={20}/>
+      <QuestPicker />
     </div>
   );
 }
