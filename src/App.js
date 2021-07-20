a7d10cdf4e976d7d0df711c6fa8b5b0855737694
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2021-07-20 12:23:15 +0300

    Add options with for loop.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header'
+import LevelPicker from './components/LevelPicker'
 
 function App() {
   return (
     <div className="App">
       <Header title='Hello'/>
+      <LevelPicker />
     </div>
   );
 }
--- src/components/LevelPicker.js
+++ src/components/LevelPicker.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import Picker from 'react-picker'
 
 const LevelPicker = () => {
+  const level = '0'
+
+  function handleLevelChange() {
+    return 0
+  }
+
+  const buildOptions = () => {
+    var arr = [];
+
+    for (let i = 1; i <= 20; i++) {
+      arr.push(<option key={i} value="{i}">{i}</option>)
+    }
+
+      return arr; 
+  }
+
   return (
     <div>
-      
+      <label for='cars'>Choose a level:</label>
+      <select name='level' id='cars'>
+        {buildOptions()}
+      </select>
     </div>
-  )
+  );
 }
 
 export default LevelPicker
