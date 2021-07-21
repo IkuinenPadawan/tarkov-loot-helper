19fe76fb0ac7a6d691a14dfea6315a478e95a5dd
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2021-07-21 18:36:41 +0300

    Align checkboxes with label.

--- src/components/QuestPicker.js
+++ src/components/QuestPicker.js
@@ -5,25 +5,47 @@ const QuestPicker = ({ quests, checkQuests }) => {
     checkQuests(parseInt(e.target.id))
   }
 
-  return (
-    <div>
-      <h4>Quests completed</h4>
-      <div>
-        {quests.map((quest) => (
-          <div>
-            <input
-              type='checkbox'
-              id={quest.id}
-              name={quest.questName}
-              value={quest.questName}
-              onChange={handleChange}
-            />
-            <label htmlFor={quest.questName}> {quest.questName} </label>
-          </div>
-        ))}
+    return (
+      <div className='quest-picker'>
+        <h4>Quests completed</h4>
+        <ul className='quest-list'>
+          {quests.map((quest) => (
+            <li>
+              <div className='quest-list-item'>
+                <input
+                  type='checkbox'
+                  id={quest.id}
+                  name={quest.questName}
+                  value={quest.questName}
+                  onChange={handleChange}
+                />
+                <label htmlFor={quest.questName}> {quest.questName} </label>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-    </div>
-  );
+    );
+
+  // return (
+  //   <div>
+  //     <h4>Quests completed</h4>
+  //     <div className='quest-picker'>
+  //       {quests.map((quest) => (
+  //         <div>
+  //           <input
+  //             type='checkbox'
+  //             id={quest.id}
+  //             name={quest.questName}
+  //             value={quest.questName}
+  //             onChange={handleChange}
+  //           />
+  //           <label htmlFor={quest.questName}> {quest.questName} </label>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default QuestPicker
