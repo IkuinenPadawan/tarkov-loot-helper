7243fe9b9b7bd4a3ed65f990ec91557ab8b19624
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2021-07-21 10:46:31 +0300

    Move quest data to App.js and create checkboxes dynamically.

--- src/App.js
+++ src/App.js
@@ -7,29 +7,53 @@ import LevelPicker from './components/LevelPicker';
 import QuestPicker from './components/QuestPicker';
 
 function App() {
+  const [quests, setQuests] = useState([
+    {
+      questName: 'Debut',
+      completed: 'false'
+    },
+    {
+      questName: 'Shortage',
+      completed: 'false'
+    },
+    {
+      questName: 'Acquintance',
+      completed: 'false'
+    },
+    {
+      questName: 'Sanitary Standards pt1',
+      completed: 'false'
+    },
+  ])
    const [items, setItems] = useState([
      {
        name: 'Salewa',
+       quest: 'shortage',
        img: 'img/Salewa.png',
      },
      {
        name: 'Mp-133',
+       quest: 'debut',
        img: 'img/Mp133.png',
      },
      {
        name: 'Iskra Lunchbox',
+       quest: 'acquintance',
        img: 'img/Lunchbox.png',
      },
      {
        name: 'Emelya Rye Croutons',
+       quest: 'acquintance',
        img: 'img/Emelya_Croutons.png',
      },
      {
        name: 'Can of Delicious Beef Stew',
+       quest: 'acquintance',
        img: 'img/Can_of_beef_stew.png',
      },
      {
        name: 'Gas Analyzer',
+       quest: 'sanitaryStandards',
        img: 'img/Gas_Analyzer.png',
      },
    ]);
@@ -37,7 +61,7 @@ function App() {
     <div className='App'>
       <Header title='Tarkov Loot Tool' />
       <LevelPicker levels={20} />
-      <QuestPicker />
+      <QuestPicker quests={quests}/>
       <ItemList items={items}/>
     </div>
   );
--- src/components/QuestPicker.js
+++ src/components/QuestPicker.js
@@ -1,28 +1,21 @@
 import React from 'react'
 
-const QuestPicker = () => {
+const QuestPicker = ({ quests }) => {
   return (
     <div>
       <h4>Quests completed</h4>
       <div>
-        <input type='checkbox' id='debut' name='debut' value='Debut' />
-        <label htmlFor='debut'> Debut</label>
-        <input type='checkbox' id='shortage' name='shortage' value='Shortage' />
-        <label htmlFor='shortage'> Shortage</label>
-        <input
-          type='checkbox'
-          id='acquintance'
-          name='acquintance'
-          value='Acquintance'
-        />
-        <label htmlFor='Acquintance'> Acquintance</label>
-        <input
-          type='checkbox'
-          id='sanitaryStandards'
-          name='sanitaryStandards'
-          value='Sanitary Standards'
-        />
-        <label htmlFor='sanitaryStandards'> Sanitary Standards</label>
+        {quests.map((quest) => (
+          <div>
+            <input
+              type='checkbox'
+              id={quest.questName}
+              name={quest.questName}
+              value={quest.questName}
+            />
+            <label htmlFor={quest.questName}> {quest.questName} </label>
+          </div>
+        ))}
       </div>
     </div>
   );
