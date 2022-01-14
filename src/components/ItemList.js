d40dede7e73169117d86ddc10a1dd5945d53051a
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2022-01-14 11:15:17 +0200

    Add initial fetch and filter from JSON. Add correct items to quests.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,12 +3,33 @@ import Item from './Item';
 
 const ItemList = ({ items, activeQuests }) => {
   console.log(activeQuests);
+  const questItems = [];
+  for (let quests of activeQuests) {
+    for (let item of quests.items) {
+      questItems.push(item);
+    }
+  }
+  // console.log(questItems);
+
+  const test = (el) => {
+    for (let item of questItems) {
+      if (item.id === el.id) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   return (
     <div>
       <ul className='item-list'>
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} img={item.img} />
-        ))}
+        {items.map((item) => {
+          return test(item) ? (
+            <Item key={item.id} name={item.name} img={item.img} />
+          ) : (
+            ''
+          );
+        })}
       </ul>
     </div>
   );
