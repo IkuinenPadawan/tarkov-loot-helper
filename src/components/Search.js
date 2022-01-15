5aab38bdf851cf08117aec3c468ced41e78ba55a
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2022-01-15 11:14:19 +0200

    Add initial css to Search field.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Search = (props) => {
-  return <div></div>;
+  return (
+    <div className='search'>
+      <label>Search</label>
+      <input />
+    </div>
+  );
 };
 
 export default Search;
