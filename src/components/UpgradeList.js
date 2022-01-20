2898cdfa631420fb93c298bcedff88462cd075f3
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2022-01-20 07:43:52 +0200

    Add UpgradeList component.
    UpgradeList and QuestList components should be combined and used
    dynamically.

---
+++ src/components/UpgradeList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HideoutUpgradePicker from './HideoutUpgradepicker';
+
+const UpgradeList = (props) => {
+  return (
+    <div>
+      <h4>Hideout upgrades completed</h4>
+      <HideoutUpgradePicker />
+    </div>
+  );
+};
+
+export default UpgradeList;
