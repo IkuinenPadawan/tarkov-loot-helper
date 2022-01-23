42fd0f36f9b670bf55f75371fc22836094e1d0bf
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2022-01-23 09:39:28 +0200

    Separate quests based on the quest giver in QuestList.js

--- src/components/QuestList.js
+++ src/components/QuestList.js
@@ -1,7 +1,54 @@
-import React from 'react'
-import QuestPicker from './QuestPicker'
+import React from 'react';
+import QuestPicker from './QuestPicker';
 
 const QuestList = ({ quests, checkQuests }) => {
+  const prapor = [];
+  const therapist = [];
+  const fence = [];
+  const skier = [];
+  const peacekeeper = [];
+  const mechanic = [];
+  const ragman = [];
+  const jaeger = [];
+
+  for (const quest of quests) {
+    switch (quest.questGiver) {
+      case 'Prapor':
+        prapor.push(quest);
+        break;
+
+      case 'Therapist':
+        therapist.push(quest);
+        break;
+
+      case 'Fence':
+        fence.push(quest);
+        break;
+
+      case 'Skier':
+        skier.push(quest);
+        break;
+
+      case 'Peacekeeper':
+        peacekeeper.push(quest);
+        break;
+
+      case 'Mechanic':
+        mechanic.push(quest);
+        break;
+
+      case 'Ragman':
+        ragman.push(quest);
+        break;
+
+      case 'Jaeger':
+        jaeger.push(quest);
+        break;
+
+      default:
+    }
+  }
+
   return (
     <div>
       <h4>Quests completed</h4>
@@ -10,4 +57,4 @@ const QuestList = ({ quests, checkQuests }) => {
   );
 };
 
-export default QuestList
+export default QuestList;
