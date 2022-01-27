3fa16d0e9422b0b40f5c7b24f2db84b252b3e9b5
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2022-01-27 11:23:10 +0200

    Add clickable tabs to determine what hideout level to show.

--- src/components/UpgradeTabs.js
+++ src/components/UpgradeTabs.js
@@ -3,10 +3,44 @@ import UpgradeList from './UpgradeList';
 
 const UpgradeTabs = ({ upgrades, checkUpgrades }) => {
   const [level, setLevel] = useState(1);
+  const [activeTab, setActiveTab] = useState(1);
+  const activeImage = {
+    backgroundImage: 'url(img/tab_hideout.png)',
+  };
+  const defaultImg = {
+    backgroundImage: 'url(img/tab_hideout.png)',
+    filter: 'brightness(50%)',
+  };
   return (
-    <div>
-      <h3>Level 1</h3>
-      <UpgradeList upgrades={upgrades} checkUpgrades={checkUpgrades} />
+    <div className='tab-container'>
+      <div
+        className='tab'
+        style={activeTab === 1 ? activeImage : defaultImg}
+        onClick={() => {
+          setActiveTab(1);
+        }}
+      >
+        <h3>Level 1</h3>
+      </div>
+      <div
+        className='tab'
+        style={activeTab === 2 ? activeImage : defaultImg}
+        onClick={() => {
+          setActiveTab(2);
+        }}
+      >
+        <h3>Level 2</h3>
+      </div>
+      <div
+        className='tab'
+        style={activeTab === 3 ? activeImage : defaultImg}
+        onClick={() => {
+          setActiveTab(3);
+        }}
+      >
+        <h3>Level 3</h3>
+      </div>
+      {/* <UpgradeList upgrades={upgrades} checkUpgrades={checkUpgrades} /> */}
     </div>
   );
 };
